944f16fd539fd360fe6c52e80bb18ed4abaff59f
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-11-30 14:32:45 +0100

    Create winner badge

--- pivni_valka/tests.py
+++ pivni_valka/tests.py
@@ -1,6 +1,7 @@
 import pytest
 
 import pivni_valka
+import utils
 
 USER_PROFILE = ''' \
 <html>
@@ -67,3 +68,49 @@ def test_parse_unique_beers_count_with_invalid_count():
         pivni_valka.parse_unique_beers_count(user_profile)
 
     assert 'invalid literal for int() with base 10' in str(excinfo.value)
+
+
+def test_winner_badge():
+    unique_beers_count_jirka = 10
+    unique_beers_count_dan = 5
+
+    page = _get_page(unique_beers_count_jirka, unique_beers_count_dan)
+
+    assert unique_beers_count_jirka > unique_beers_count_dan
+    assert 'Jirka 👑' in page
+    assert 'Dan 👑' not in page
+
+
+def test_winner_badge_dan():
+    unique_beers_count_jirka = 5
+    unique_beers_count_dan = 10
+
+    page = _get_page(unique_beers_count_jirka, unique_beers_count_dan)
+
+    assert unique_beers_count_jirka < unique_beers_count_dan
+    assert 'Jirka 👑' not in page
+    assert 'Dan 👑' in page
+
+
+def test_no_winner_badge():
+    unique_beers_count_jirka = 5
+    unique_beers_count_dan = 5
+
+    page = _get_page(unique_beers_count_jirka, unique_beers_count_dan)
+
+    assert unique_beers_count_jirka == unique_beers_count_dan
+    assert 'Jirka 👑' not in page
+    assert 'Dan 👑' not in page
+
+
+def _get_page(unique_beers_count_jirka, unique_beers_count_dan):
+    return pivni_valka.get_page(
+        utils.get_template('pivni-valka.html', '../templates'),
+        unique_beers_count_jirka=unique_beers_count_jirka,
+        unique_beers_count_dan=unique_beers_count_dan,
+        chart_labels=[],
+        chart_data_jirka=[],
+        chart_data_dan=[],
+        diff_jirka='',
+        diff_dan='',
+    )
