b926710e6f681ea18106a022d6b673cdedbb0c78
Jiri Syrovy <jiri.syrovy@gmail.com>
2022-10-19 23:42:09 +0200

    Refactor robot

--- robot/db.py
+++ robot/db.py
@@ -3,13 +3,10 @@ from robot.base import BaseRobot
 
 
 class DbRobot(BaseRobot):
-    def __init__(self) -> None:
-        self._db = db
-
     def run(self) -> None:
         super().run()
-        self._db.dump()
-        self._db.close()
+        db.dump()
+        db.close()
 
     def _main(self) -> None:
         raise NotImplementedError
