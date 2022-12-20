4ddaa2d5d9e5cbe701000d52992209656b58e213
Jiri Syrovy <jiri.syrovy@gmail.com>
2022-12-20 08:03:50 +0100

    Update test data

--- pivni_valka/tests/stats/test_common.py
+++ pivni_valka/tests/stats/test_common.py
@@ -2,4 +2,9 @@ from pivni_valka.stats.common import get_total_unique_beers
 
 
 def test_get_total_unique_beers():
-    assert get_total_unique_beers() == {'Mates511': 43, 'mencik2': 39, 'ominar': 47, 'sejrik': 35}
+    assert get_total_unique_beers() == {
+        "sejrik": 11,
+        "mencik2": 22,
+        "Mates511": 33,
+        "ominar": 0,
+    }
