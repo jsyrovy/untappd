50d5ecf8d62febb6672c7341827c4435e0fff05c
Jiri Syrovy <jiri.syrovy@gmail.com>
2022-12-20 08:45:51 +0100

    Update tests

--- pivni_valka/tests/stats/test_common.py
+++ pivni_valka/tests/stats/test_common.py
@@ -1,4 +1,10 @@
-from pivni_valka.stats.common import get_total_unique_beers
+import datetime
+
+from pivni_valka.stats.common import (
+    get_total_unique_beers,
+    get_unique_beers,
+    get_unique_beers_before,
+)
 
 
 def test_get_total_unique_beers():
@@ -8,3 +14,17 @@ def test_get_total_unique_beers():
         "Mates511": 33,
         "ominar": 0,
     }
+
+
+def test_get_unique_beers():
+    assert get_unique_beers("sejrik") == "11"
+    assert get_unique_beers("sejrik", days=1) == "10"
+    assert get_unique_beers("sejrik", days=1, formatted=True) == "+10"
+    assert get_unique_beers("ominar") == "0"
+    assert get_unique_beers("ominar", formatted=True) == "0"
+
+
+def test_get_unique_beers_before():
+    assert get_unique_beers_before("sejrik", datetime.date.today()) == 11
+    assert get_unique_beers_before("sejrik", datetime.date(2022, 1, 2)) == 0
+    assert get_unique_beers_before("sejrik", datetime.date(2022, 1, 3)) == 1
