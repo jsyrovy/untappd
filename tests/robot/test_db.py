f859df380641a94e979712bdc7fdde9503f01750
Jiri Syrovy <jiri.syrovy@gmail.com>
2023-01-04 14:13:39 +0100

    Tests

--- tests/robot/test_db.py
+++ tests/robot/test_db.py
@@ -1,3 +1,5 @@
+from sqlite3 import ProgrammingError
+
 import mock
 import pytest
 
@@ -5,6 +7,15 @@ from db import db
 from robot.db import DbRobot
 
 
+def use_fresh_db(func):
+    def wrapper():
+        db.__init__(use_test_db=True)
+        func()
+
+    return wrapper
+
+
+@use_fresh_db
 def test_db_robot():
     robot = DbRobot()
     robot._main = mock.Mock()
@@ -15,7 +26,27 @@ def test_db_robot():
             close_mock.assert_called_once()
 
 
+@use_fresh_db
 def test_base_robot_with_error():
     robot = DbRobot()
     with pytest.raises(NotImplementedError):
         robot.run()
+
+
+@use_fresh_db
+def test_dump():
+    with mock.patch('builtins.open', mock.mock_open()) as dump_mock:
+        db.dump()
+        dump_mock.assert_called_once_with('data/data_dump.sql', 'w', encoding='utf-8')
+
+
+@use_fresh_db
+def test_close():
+    db.close()
+    with pytest.raises(ProgrammingError):
+        db.con.execute("select 1")
+
+
+@use_fresh_db
+def test_commit():
+    ...
