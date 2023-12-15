fbd5c52781a26496af696146f72db85a4c39584b
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2023-12-15 10:51:45 +0100

    Revert "Revert "Add Kaja to history""
    This reverts commit 7d77d8fe

--- utils/user.py
+++ utils/user.py
@@ -14,6 +14,7 @@ USERS = (
     User("Dan", "mencik2", "#43aa8b"),
     User("Matěj", "Mates511", "#90be6d"),
     User("Ondra", "ominar", "#f9c74f", hidden=True),
+    User("Kája", "karolina_matukova_7117", "#f88379"),
 )
 USER_NAMES = tuple(user.user_name for user in USERS)
 
