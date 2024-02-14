65bfb3c79da14dc93f6b37ff6141af34810818e5
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2024-02-14 18:48:19 +0100

    Tests

---
+++ tests/database/test_utils.py
@@ -0,0 +1,12 @@
+import mock
+
+from database.orm import engine
+from database.utils import DUMP_PATH, dump
+
+
+def test_dump():
+    with mock.patch("database.utils.open", mock.mock_open()) as mocked_open:
+        dump(engine)
+
+    mocked_open.assert_called_once_with(DUMP_PATH, "w", encoding="utf-8")
+    mocked_open().write.assert_called()
