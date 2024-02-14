c1b79e0a189fa7ab461a3c5545e5c1316665ec2f
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2024-02-14 18:36:56 +0100

    SA

--- pivni_valka/stats/weekly_chart.py
+++ pivni_valka/stats/weekly_chart.py
@@ -1,29 +1,29 @@
 import datetime
 from collections import OrderedDict
-from typing import Sequence
 
-from sqlalchemy import text, Row
+from sqlalchemy import select, func
+from sqlalchemy.orm import Session
 
 import utils.user
+from database.models import PivniValka
 from database.orm import engine
 from pivni_valka.stats.common import ChartData, ChartDataset
 
 
 def _get_week_stats() -> dict[str, dict[str, int]]:
-    since = (datetime.date.today() - datetime.timedelta(weeks=12)).strftime("%Y-%W")
+    since = datetime.date.today() - datetime.timedelta(weeks=12)
     result: dict[str, dict[str, int]] = {}
-    with engine.connect() as conn:
-        stats: Sequence[Row[tuple[str, str, int]]] = conn.execute(
-            text(
-                """
-                SELECT strftime('%Y-%W', `date`) as week, user, sum(unique_beers)
-                FROM pivni_valka
-                WHERE week >= :week
-                GROUP BY week, user
-                ORDER BY week
-            """,
-            ).bindparams(week=since)
-        ).all()
+    stmt = (
+        select(
+            func.strftime("%Y-%W", PivniValka.date).label("week"), PivniValka.user, func.sum(PivniValka.unique_beers)
+        )
+        .where(PivniValka.date >= since)
+        .group_by("week", "user")
+        .order_by("week")
+    )
+
+    with Session(engine) as session:
+        stats = session.execute(stmt).all()
 
     for week, user_name, beers in stats:
         result.setdefault(user_name, OrderedDict())[week] = beers
