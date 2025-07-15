0f1f975f4b0b3cb6bfbb530e7cf2914eeae6dde6
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-07-15 18:24:34 +0200

    Refactor test conf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
 import pytest
 
-from database.models import Archive, PivniValka
+from database.models import Base
 from database.orm import engine
 from database.utils import load_dump
 
 
 @pytest.fixture(autouse=True)
 def use_fresh_test_db():
-    PivniValka.__table__.drop(engine)
-    Archive.__table__.drop(engine)
+    Base.metadata.drop_all(engine)
     load_dump(engine, use_test_db=True)
